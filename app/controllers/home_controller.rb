cca0ce9d60fb8818363c278a8e3e02e403f43e0a
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2017-12-05 22:58:22 +0100

    WIp

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
-    render plain: 'ok'
+    redis = Redis.new(url: ENV.fetch('REDIS_URL'))
+    store = Redis::Namespace.new(ns: 'api_rate', redis: redis)
+    next_count = Integer(store.get('count') || 0) + 1
+    store.set('count', next_count)
+
+    render plain: 'ok' + store.get('count')
   end
 end
